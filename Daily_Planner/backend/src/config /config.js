2db8ba7b1d722da990fb7a109aa59cebf70e780d
keshav <keshavrokaya1244@gmail.com>
2026-02-06 13:38:41 +0545

    adding database connection logic

---
+++ Daily_Planner/backend/src/config /config.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+    PORT: process.env.PORT || 5000,
+    Mongodb_url: process.env.Mongodb_url || "mongodb://localhost:27017/dailyplanner",
+    jwt_secret: process.env.jwt_secret,
+    jwt_expiration: process.env.jwt_expiration,
+};
+
+module.exports = config;
---
+++ Daily_Planner/backend/src/config /db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+import mongoose from "mongoose";
+const config = require('./config');
+const db = {};
+
+
+db.connect = async () =>{
+    try{
+        await mongoose.connect(config.Mongodb_url);
+        console.log("Database connect successfully.");
+    }catch (error){
+        console.log("Data connection failed",error)
+
+    }
+};
+
+db.disconnect = async () =>{
+    try{
+        await mongoose.connection.close();
+        console.log("Database disconnect successfully.");
+    }catch (error){
+        console.log("Database disconnect failed.", error)
+    }
+};
+
+module.exports = db;
